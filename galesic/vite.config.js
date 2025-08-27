4f264857521e882a84013a6486b6b64cd58bca61
Mirza <mirza-hrustanovic@hotmail.com>
2025-08-27 12:28:19 +0200

    new adding

--- galesic/vite.config.js
+++ galesic/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/GalesicGradnja",
+  base: "/GalesicGradnja/",
   server: {
     watch: {
       usePolling: true,
